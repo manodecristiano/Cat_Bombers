3734ba4b8b4cbe75b6134db8ad35d3c391586f1c
manodecristiano <cristian.carre.online@gmail.com>
2022-11-07 02:10:36 +0100

    Home page

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,112 @@
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Homepage extends StatelessWidget {
+  const Homepage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(249, 245, 229, 1.0),
+      // appBar: AppBar(
+      //   elevation: 5,
+      //   backgroundColor: Colors.amber[200],
+      // ),
+      body: Center(
+        child: Column(children: [
+          Container(
+            margin: const EdgeInsets.only(
+              top: 30,
+            ),
+            // ignore: prefer_const_constructors
+            child: AutoSizeText(
+              'Cat-BOMBERS',
+              style: TextStyle(
+                color: Color.fromRGBO(236, 194, 44, 1.0),
+                fontWeight: FontWeight.bold,
+                fontFamily: 'Roboto',
+              ),
+              maxLines: 1,
+              minFontSize: 48,
+              maxFontSize: 100,
+            ),
+          ),
+          Card(
+            margin: const EdgeInsets.all(30),
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+            color: Color.fromRGBO(249, 240, 229, 1.0),
+            child: ListView(
+              shrinkWrap: true,
+              padding: const EdgeInsets.all(10),
+              children: [
+                Container(
+                  height: 50,
+                  margin: const EdgeInsets.only(top: 10),
+                  child: const Center(
+                    child: AutoSizeText(
+                      'Menú',
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontWeight: FontWeight.bold,
+                        fontFamily: 'Roboto',
+                      ),
+                      maxLines: 1,
+                      minFontSize: 40,
+                      maxFontSize: 100,
+                    ),
+                  ),
+                ),
+                Divider(),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const AutoSizeText(
+                    'TEST RÁPIDO',
+                    style: TextStyle(
+                      color: Color.fromRGBO(239, 71, 111, 1.0),
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                    maxLines: 1,
+                    minFontSize: 20,
+                    maxFontSize: 100,
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(239, 222, 150, 1.0),
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    elevation: 8,
+                    side: const BorderSide(width: 0.2),
+                  ),
+                ),
+                Divider(),
+                Divider(),
+                OutlinedButton(
+                  onPressed: () {},
+                  child: const AutoSizeText(
+                    'Tipo de TEST',
+                    style: TextStyle(
+                      color: Colors.black87,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Roboto',
+                    ),
+                    maxLines: 1,
+                    minFontSize: 20,
+                    maxFontSize: 100,
+                  ),
+                  style: OutlinedButton.styleFrom(
+                    backgroundColor: Color.fromRGBO(249, 245, 229, 1.0),
+                    padding: const EdgeInsets.fromLTRB(0, 15, 0, 15),
+                    elevation: 3,
+                    side: const BorderSide(width: 0.2),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
