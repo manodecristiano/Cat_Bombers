3762dbf0494747d9bf04fa4084cdb1928852e284
manodecristiano <cristian.carre.online@gmail.com>
2022-12-19 00:58:18 +0100

    Perfil Update

--- lib/pages/perfil_user.dart
+++ lib/pages/perfil_user.dart
@@ -40,60 +40,221 @@ class _Perfil_user extends State<Perfil_user> {
         elevation: 5,
         backgroundColor: Colors.amber[200],
       ),
-      body: Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-        ConstrainedBox(
-            constraints: const BoxConstraints(maxHeight: 450),
-            child: Card(
-              margin: const EdgeInsets.symmetric(horizontal: 18, vertical: 15),
-              child: Column(children: [
-                SizedBox(
-                  height: 250.0,
-                  width: 340,
-                  child: Image.asset('assets/img/bombers.png'),
-                )
-              ]),
-            )),
-        ConstrainedBox(
-          constraints: const BoxConstraints(maxHeight: 450),
-          child: PieChart(
-            PieChartData(
-                borderData: FlBorderData(
-                  show: false,
-                ),
-                sectionsSpace: 0,
-                centerSpaceRadius: 0,
-                sections: sectionsChart),
-          ),
+      body: Center(
+        child: Column(
+          children: [
+            Card(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12)),
+              color: Color.fromRGBO(249, 240, 229, 1.0),
+              margin:
+                  const EdgeInsets.only(left: 2, right: 2, top: 2, bottom: 10),
+            ),
+            AutoSizeText('NIVEL: 57',
+                minFontSize: 25,
+                maxFontSize: 35,
+                style: TextStyle(
+                    color: Colors.brown, fontWeight: FontWeight.bold)),
+            Text('Nombre : Carlos Messi',
+                style: TextStyle(color: Colors.brown)),
+            Text('Test Realizados:85'),
+            Text('Total de preguntas: 345'),
+            Expanded(
+              child: PieChart(
+                PieChartData(
+                    borderData: FlBorderData(
+                      show: false,
+                    ),
+                    sectionsSpace: 0,
+                    centerSpaceRadius: 0,
+                    sections: sectionsChart),
+              ),
+            ),
+          ],
         ),
-      ]),
+      ),
     );
   }
 
   List<PieChartSectionData> sectionsChart = [
     PieChartSectionData(
-      value: 35,
-      title: "35%",
+      value: 4.5,
+      title: "Hidráulica",
       showTitle: true,
       color: Colors.orange,
       radius: 100,
     ),
     PieChartSectionData(
-      value: 45,
-      title: "45%",
+      value: 4.5,
+      title: "Automoción",
       showTitle: true,
       color: Colors.blue,
       radius: 100,
     ),
     PieChartSectionData(
-      value: 15,
-      title: "15%",
+      value: 4.5,
+      title: "Electricidad",
       showTitle: true,
       color: Colors.red,
       radius: 100,
     ),
     PieChartSectionData(
-      value: 5,
-      title: "5%",
+      value: 4.5,
+      title: "Fuego",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Constitució",
+      showTitle: true,
+      color: Colors.orange,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Igualdad",
+      showTitle: true,
+      color: Colors.blue,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Estatut",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Mecánica",
+      showTitle: true,
+      color: Colors.orange,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Construcció",
+      showTitle: true,
+      color: Colors.blue,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Química",
+      showTitle: true,
+      color: Colors.red,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Ley 5/1994",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "R.Químic",
+      showTitle: true,
+      color: Colors.orange,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Trebep",
+      showTitle: true,
+      color: Colors.blue,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Sanitari",
+      showTitle: true,
+      color: Colors.red,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "R.Laborales",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "R.Químic",
+      showTitle: true,
+      color: Colors.orange,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Estructura",
+      showTitle: true,
+      color: Colors.blue,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Física",
+      showTitle: true,
+      color: Colors.red,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Meteorologia",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "P.Civil",
+      showTitle: true,
+      color: Colors.black26,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "A.Extintores",
+      showTitle: true,
+      color: Colors.orange,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Cartografia",
+      showTitle: true,
+      color: Colors.blue,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "P.Incendios",
+      showTitle: true,
+      color: Colors.red,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Guardia",
+      showTitle: true,
+      color: Colors.purple,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Gas",
+      showTitle: true,
+      color: Colors.red,
+      radius: 100,
+    ),
+    PieChartSectionData(
+      value: 4.5,
+      title: "Territori",
       showTitle: true,
       color: Colors.purple,
       radius: 100,
