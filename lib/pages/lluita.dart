c2235106fa272ba5cebe62bc3d3f30808db07981
Cristian <Cristian.carre.online@gmail.com>
2024-11-25 00:40:46 +0100

    Update lluita.dart

--- lib/pages/lluita.dart
+++ lib/pages/lluita.dart
@@ -15,9 +15,11 @@ class LLuita extends StatefulWidget {
 
 class _LLuita extends State<LLuita> {
   int totalOptions = 4;
-  int totaldePreguntas = 15;
+  int totaldePreguntas = 5;
   bool mostrarRespuestasTest1 = false;
   bool mostrarRespuestasTest2 = false;
+  bool test1Terminado = false;
+  bool test2Terminado = false;
 
   int touchedOptionIndex = -1; // Índice de la opción tocada (naranja)
   int selectedOptionIndex = -1; // Índice de la opción seleccionada (verde)
@@ -42,20 +44,44 @@ class _LLuita extends State<LLuita> {
       Pregunta.fromJson({
         'id': 0,
         'categoria': 'Hidráulica',
-        'pregunta':
-            "¿Quina de les següents propietats de l'aigua és una bona qualitat per extingir incendis,Quina de les següents propietats de l'aigua és una bona qualitat per extingir incendis?",
-        'respuesta': "L'aigua pura Correcta no conduiex l'electricitat  és una bona qualitat per extingir"
+        'pregunta': "¿En que país no hemos estado ninguno de los 2??",
+        'respuesta': "Bélgica"
       }),
-      Pregunta.fromJson(
-          {'id': 1, 'categoria': 'Electricitat', 'pregunta': 'España es el mejor país?', 'respuesta': 'Correcta'}),
-      Pregunta.fromJson({'id': 2, 'categoria': 'Hidráulica', 'pregunta': 'Donde vives?', 'respuesta': 'Correcta'}),
-      Pregunta.fromJson(
-          {'id': 3, 'categoria': 'Hidráulica', 'pregunta': 'Cuántas estrellas existen?', 'respuesta': 'Correcta'}),
-      Pregunta.fromJson(
-          {'id': 4, 'categoria': 'Hidráulica', 'pregunta': 'Después del 4 viene el...?', 'respuesta': 'Correcta'}),
-      Pregunta.fromJson(
-          {'id': 5, 'categoria': 'Electricitat', 'pregunta': '5 por el culo...?', 'respuesta': 'Correcta'}),
-      Pregunta.fromJson(
+      Pregunta.fromJson({
+        'id': 1,
+        'categoria': 'Electricitat',
+        'pregunta': '¿Que día empezamos nuestra relación sentimental?',
+        'respuesta': 'treinta de marzo de 2023'
+      }),
+      Pregunta.fromJson({
+        'id': 2,
+        'categoria': 'Hidráulica',
+        'pregunta': '¿Cuál es el país con más hablantes nativos de español en el mundo?',
+        'respuesta': 'México'
+      }),
+      Pregunta.fromJson({
+        'id': 3,
+        'categoria': 'Hidráulica',
+        'pregunta':
+            ': ¿Qué país es conocido como la cuna del Renacimiento y qué ciudad fue su principal centro cultural?',
+        'respuesta': ' Italia'
+      }),
+      Pregunta.fromJson({
+        'id': 4,
+        'categoria': 'Hidráulica',
+        'pregunta': '¿En qué país se originaron los Juegos Olímpicos antiguos y qué ciudad los albergaba?',
+        'respuesta': 'Los Juegos Olímpicos antiguos se originaron en Grecia y se celebraban en la ciudad de Olimpia.'
+      }),
+      Pregunta.fromJson({
+        'id': 5,
+        'categoria': 'Electricitat',
+        'pregunta':
+            '¿Qué monumento histórico fue construido como una tumba en la India y es considerado una de las siete maravillas del mundo moderno?',
+        'respuesta':
+            'El Taj Mahal fue construido como una tumba en la India y es una de las siete maravillas del mundo moderno.'
+      }),
+
+      /*  Pregunta.fromJson(
           {'id': 6, 'categoria': 'Hidráulica', 'pregunta': 'Helicóptero lleva H?', 'respuesta': 'Correcta'}),
       Pregunta.fromJson({
         'id': 7,
@@ -83,7 +109,7 @@ class _LLuita extends State<LLuita> {
       Pregunta.fromJson(
           {'id': 18, 'categoria': 'Electricitat', 'pregunta': 'De qué color es el verde?', 'respuesta': 'Correcta'}),
       Pregunta.fromJson(
-          {'id': 19, 'categoria': 'Hidráulica', 'pregunta': 'Tu actor favorito?', 'respuesta': 'Correcta'}),
+          {'id': 19, 'categoria': 'Hidráulica', 'pregunta': 'Tu actor favorito?', 'respuesta': 'Correcta'}), */
     ];
 
     // Maneja la interacción al tocar una opción
@@ -148,6 +174,10 @@ class _LLuita extends State<LLuita> {
       if (test1Index < test1Preguntas.preguntas.length - 1) {
         test1Index++;
         test1Progress++;
+        mostrarRespuestasTest1 = false;
+      } else {
+        test1Terminado = true;
+        if (test2Terminado) _mostrarResultadoFinal();
       }
     });
   }
@@ -161,6 +191,10 @@ class _LLuita extends State<LLuita> {
       if (test2Index < test2Preguntas.preguntas.length - 1) {
         test2Index++;
         test2Progress++;
+        mostrarRespuestasTest2 = false;
+      } else {
+        test2Terminado = true;
+        if (test1Terminado) _mostrarResultadoFinal();
       }
     });
   }
@@ -183,8 +217,8 @@ class _LLuita extends State<LLuita> {
 
 //** ----------AL FINAL DEL TEST MOSTRAMOS UN RESULTADO-------------------------------
 
-  /*  Widget resultadoALert(BuildContext context) {
-    int porcentaje = ((todasLasPreguntas.right / totaldePreguntas) * 100).toInt();
+  Widget resultadoALert(BuildContext context) {
+    int porcentaje = ((test1Preguntas.right / totaldePreguntas) * 100).toInt();
     int numPreguntas = totaldePreguntas;
 
     return AlertDialog(
@@ -194,12 +228,12 @@ class _LLuita extends State<LLuita> {
         mainAxisSize: MainAxisSize.min,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Text('Preguntas  :  ' '${totaldePreguntas}'),
-          Text('Correctas   :  ' '${todasLasPreguntas.right}',
+          Text('Preguntas  :  ' '${test1Preguntas}'),
+          Text('Correctas   :  ' '${test1Preguntas.right}',
               style: const TextStyle(color: Colors.greenAccent, fontWeight: FontWeight.bold)),
           Text(
               'Incorrectas:  '
-              '${(totaldePreguntas - todasLasPreguntas.right)}',
+              '${(totaldePreguntas - test1Preguntas.right)}',
               style: const TextStyle(color: Colors.redAccent, fontWeight: FontWeight.bold)),
           Text('Porcentaje :  ' '$porcentaje%'),
         ],
@@ -211,8 +245,8 @@ class _LLuita extends State<LLuita> {
               Navigator.pushReplacement(
                 context,
                 MaterialPageRoute(
-                  builder: ((context) => ResultadoTest(
-                      questionario: todasLasPreguntas, numPreguntas: numPreguntas, porcentaje: porcentaje)),
+                  builder: ((context) =>
+                      ResultadoTest(questionario: test1Preguntas, numPreguntas: numPreguntas, porcentaje: porcentaje)),
                 ),
               );
             },
@@ -239,7 +273,63 @@ class _LLuita extends State<LLuita> {
         ),
       ],
     );
-  } */
+  }
+
+  void _mostrarResultadoFinal() {
+    // Verificar si ambos tests han terminado
+    if (test1Index == test1Preguntas.preguntas.length - 1 && test2Index == test2Preguntas.preguntas.length - 1) {
+      String ganador;
+      if (test1RightAnswers > test2RightAnswers) {
+        ganador = "¡El jugador 1 ganó!";
+      } else if (test1RightAnswers < test2RightAnswers) {
+        ganador = "¡El jugador 2 ganó!";
+      } else {
+        ganador = "¡Es un empate!";
+      }
+
+      showDialog(
+        context: context,
+        barrierDismissible: false,
+        builder: (BuildContext context) {
+          return AlertDialog(
+            title: const Text(
+              "Resultado Final",
+              style: TextStyle(fontWeight: FontWeight.bold),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Text("Jugador 1: $test1RightAnswers respuestas correctas"),
+                Text("Jugador 2: $test2RightAnswers respuestas correctas"),
+                const SizedBox(height: 10),
+                Text(
+                  ganador,
+                  style: const TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                    color: Colors.green,
+                  ),
+                ),
+              ],
+            ),
+            actions: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).pop();
+                  Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(
+                      builder: (context) => const Homepage(),
+                    ),
+                  );
+                },
+                child: const Text("Volver al inicio"),
+              ),
+            ],
+          );
+        },
+      );
+    }
+  }
 
 //** ----------------------------------------------------------------*
 
@@ -250,72 +340,81 @@ class _LLuita extends State<LLuita> {
     required bool mostrarRespuestas,
     required Function() toggleMostrarRespuestas,
     required Function(String) onOptionSelected,
+    required bool terminado,
   }) {
     return Column(
       children: [
-        // Pregunta (o ícono '?')
-        GestureDetector(
-          onTap: toggleMostrarRespuestas, // Alternar entre pregunta y respuestas
-          child: Container(
-            margin: const EdgeInsets.only(
-              top: 5,
-              bottom: 0,
-              left: 18,
-              right: 18,
+        if (terminado)
+          const Center(
+            child: Text(
+              "Esperando...",
+              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18, color: Colors.red),
             ),
-            child: Card(
-              color: const Color.fromRGBO(250, 235, 215, 1.0),
-              child: Padding(
-                padding: const EdgeInsets.only(
-                  top: 1, // Margen superior
-                  bottom: 0,
-                  left: 18,
-                  right: 18,
-                ),
-                child: Center(
-                  child: mostrarRespuestas
-                      ? // Progreso
-                      Container(
-                          margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(15),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween, // Separar elementos
-                              children: [
-                                Expanded(
-                                  flex: 4, // Espacio proporcional para el progreso
-                                  child: LinearProgressIndicator(
-                                    color: Colors.amber[700],
-                                    backgroundColor: const Color(0xFFD9D9D9),
-                                    value: (testIndex + 1) / testPreguntas.preguntas.length,
-                                    minHeight: 20,
+          )
+        else
+          // Pregunta (o ícono '?')
+          GestureDetector(
+            onTap: toggleMostrarRespuestas, // Alternar entre pregunta y respuestas
+            child: Container(
+              margin: const EdgeInsets.only(
+                top: 5,
+                bottom: 0,
+                left: 18,
+                right: 18,
+              ),
+              child: Card(
+                color: const Color.fromRGBO(250, 235, 215, 1.0),
+                child: Padding(
+                  padding: const EdgeInsets.only(
+                    top: 1, // Margen superior
+                    bottom: 0,
+                    left: 18,
+                    right: 18,
+                  ),
+                  child: Center(
+                    child: mostrarRespuestas
+                        ? // Progreso
+                        Container(
+                            margin: const EdgeInsets.symmetric(horizontal: 30, vertical: 2),
+                            child: ClipRRect(
+                              borderRadius: BorderRadius.circular(15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Separar elementos
+                                children: [
+                                  Expanded(
+                                    flex: 4, // Espacio proporcional para el progreso
+                                    child: LinearProgressIndicator(
+                                      color: Colors.amber[700],
+                                      backgroundColor: const Color(0xFFD9D9D9),
+                                      value: (testIndex + 1) / testPreguntas.preguntas.length,
+                                      minHeight: 20,
+                                    ),
                                   ),
-                                ),
-                                const SizedBox(width: 10), // Espaciado entre progreso e ícono
-                                const Icon(
-                                  Icons.help_outline, // Ícono '?'
-                                  size: 24,
-                                  color: Colors.black54,
-                                ),
-                              ],
+                                  const SizedBox(width: 10), // Espaciado entre progreso e ícono
+                                  const Icon(
+                                    Icons.help_outline, // Ícono '?'
+                                    size: 24,
+                                    color: Colors.black54,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          )
+                        : Text(
+                            testPreguntas.preguntas[testIndex].pregunta,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 16,
                             ),
                           ),
-                        )
-                      : Text(
-                          testPreguntas.preguntas[testIndex].pregunta,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                            fontWeight: FontWeight.bold,
-                            fontSize: 16,
-                          ),
-                        ),
+                  ),
                 ),
               ),
             ),
           ),
-        ),
         // Respuestas (se muestran si `mostrarRespuestas` es true)
-        if (mostrarRespuestas)
+        if (mostrarRespuestas && !terminado)
           Flexible(
             child: ListView.builder(
               itemCount: totalOptions,
@@ -369,6 +468,7 @@ class _LLuita extends State<LLuita> {
                     mostrarRespuestasTest1 = !mostrarRespuestasTest1;
                   });
                 },
+                terminado: test1Terminado,
                 onOptionSelected: _handleOptionSelectedTest1,
               ),
             ),
@@ -410,6 +510,7 @@ class _LLuita extends State<LLuita> {
                   mostrarRespuestasTest2 = !mostrarRespuestasTest2;
                 });
               },
+              terminado: test2Terminado,
               onOptionSelected: _handleOptionSelectedTest2,
             ),
           ),
